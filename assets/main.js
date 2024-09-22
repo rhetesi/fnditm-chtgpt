ae37cd4dd996db56995eeecfc765f04623d75c23
rhetesi <66908912+rhetesi@users.noreply.github.com>
2024-09-22 20:55:11 +0000

    Rename items variable to itemsEntity

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const apiUrl = 'https://my-json-server.typicode.com/rhetesi/fnditm-chtgpt';
-const items = 'items';
+const itemsEntity = 'items';
 
 const getItemsfromAPI = async () => {
     try {
-      const response = await fetch(`${apiUrl}/${items}`);
+      const response = await fetch(`${apiUrl}/${itemsEntity}`);
       const result = await response.json();
       console.log(result);
     } catch (error) {
