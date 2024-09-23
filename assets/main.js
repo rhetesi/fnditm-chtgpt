71b865d5bac3d8b2c67ca745e9c9d3c3f026db84
rhetesi <66908912+rhetesi@users.noreply.github.com>
2024-09-23 19:36:18 +0000

    Add some comment & usersEntity and usersList array

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,15 @@
 'use strict';
-import apiUrl  from './services/config.js';
+// import apiUrl  from './services/config.js';
 import { getAll, get } from './services/base.js';
 
 // const apiUrl = 'https://my-json-server.typicode.com/rhetesi/fnditm-chtgpt';
+
+// Hogy NE lehessen kívülről hozzáférni semmilyen változóhoz, ezeket NEM itt kell majd definiálni
+// hanem az adott funkciót megvalósító blokk elején, s ott ráadásul elég lesz az 'entity' változó használata
 const itemsEntity = 'items';
+const usersEntity = 'users';
 let itemsList = [];
+let usersList = [];
 // let itemsList2 = [];
 
 const getList = async (entity, array) => {
@@ -21,6 +26,9 @@ const getList = async (entity, array) => {
     // return array = internArray.map(element => element);
 }
 
+// A getList() megfelelő entity és array paraméterek meghívásával
+// az API felől érkező elemek a hívásban szereplő tömbbe kerülnek
+// itt az itemsEntity (entity) és az ItemsList (tömb)
 getList(itemsEntity, itemsList);
 console.log(itemsList);
 // console.log(itemsList2);
